9d127f537850fa6b00c93fa576756f59894ca4aa
Sumaya <sheikhsumaya622@gmail.com>
2026-06-19 19:27:00 +0600

    navber settings

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,13 +5,13 @@ const Navbar = () => {
         <>
             <nav class="navbar navbar-dark bg-card fixed-top">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Offcanvas dark navbar</a>
+                    <a class="navbar-brand" href="#">Sumu's All Courses</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
                         <div class="offcanvas-header">
-                            <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Dark offcanvas</h5>
+                            <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Sumu's All Courses</h5>
                             <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                         </div>
                         <div class="offcanvas-body">
@@ -20,19 +20,45 @@ const Navbar = () => {
                                     <a class="nav-link active" aria-current="page" href="#">Home</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="#">Link</a>
+                                    <a class="nav-link" href="#">HTML</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">CSS</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">JavaScript</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">React</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Github</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Typescript</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Next.js</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">PostgreSQL</a>
                                 </li>
                                 <li class="nav-item dropdown">
                                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        Dropdown
+                                        Actions
                                     </a>
                                     <ul class="dropdown-menu dropdown-menu-dark">
-                                        <li><a class="dropdown-item" href="#">Action</a></li>
-                                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                                        <li><a class="dropdown-item" href="#">Strikes</a></li>
+                                        <li><a class="dropdown-item" href="#">Note</a></li>
+                                        <li><a class="dropdown-item" href="#">Links</a></li>
+                                        <li><a class="dropdown-item" href="#">Saved</a></li>
+                                        <li><a class="dropdown-item" href="#">About Me</a></li>
+                                        <li><a class="dropdown-item" href="#">Blog</a></li>
+                                        <li><a class="dropdown-item" href="#">Settings</a></li>
                                         <li>
                                             <hr class="dropdown-divider" />
                                         </li>
-                                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                        <li><a class="dropdown-item" href="#">Profile</a></li>
                                     </ul>
                                 </li>
                             </ul>
